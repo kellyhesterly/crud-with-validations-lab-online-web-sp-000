5522810799ce770a024a336fe368c5fb9cb4338e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-29 16:50:58 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,6 +12,13 @@ class SongsController < ApplicationController
   end
 
   def create
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
